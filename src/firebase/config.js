8e7716b62da78c5a015e06201d1ad61e171e6adc
rahul055 <rrjoshi055@gmail.com>
2021-11-17 19:54:21 +0530

    skeliton of app

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCqdbg-564bt2lIHuTDTEnR_4vPUjyD-SA",
+  authDomain: "firebook-react.firebaseapp.com",
+  projectId: "firebook-react",
+  storageBucket: "firebook-react.appspot.com",
+  messagingSenderId: "255027997837",
+  appId: "1:255027997837:web:e904a57a1d503850a8c6c4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
